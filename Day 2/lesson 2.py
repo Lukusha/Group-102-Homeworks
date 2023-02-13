edabca897f56ab5cac850e73858899d207427f01
Lukusha <luka.beraia808@gmail.com>
2023-02-13 11:57:30 +0400

    day 2 commit

---
+++ Day 2/lesson 2.py
@@ -0,0 +1,61 @@
+#name = "Luka"
+#print(len(name))
+
+#my_sentence = """gamarjoba shegaxsenebt,rom swavlas iwyebt 19 seqtembers
+#ar daagvianot gakvetilze
+#da ar gaacdinot gakvetilebi""" # grdzel stringebs,ramdenime xazian stringebs undat sami """
+
+#print(len(my_sentence))
+
+#print(my_sentence)
+
+#knows_programming = True
+
+#if False:
+#    print("hello")
+#    #tab - aris indentacia ( indentation )
+
+
+#full_name = "Luka Beraia"
+#print(len(full_name))
+#position = index
+#yvela stringi sheidzleba mivichniot siad
+#print(full_name[0])
+#print(full_name[5])
+
+#print("L" not in full_name)
+
+full_name = "Luka Beraia"
+# print(full_name [2:9])
+# print(full_name[2:9:2])
+# [start:finish:step]
+# print(full_name[:5])
+# print(full_name[5:])
+
+
+# print(full_name[-1])#bolo asos gamoitans -1
+# print(full_name[-4] + "oar")
+# print(full_name[-1:-6:-2])
+
+
+#stringebs aqvt metodebi
+#   1)string.upper()
+#   2)len(string)
+
+
+#print(full_name.upper())
+
+name2 = "       LUKA       "
+#print(name2.lower())
+
+#print(name2.strip())
+
+#print(name2.replace(" ","#"))
+
+
+#print(name2.replace("A","#"))
+
+name3 = "lukkakkaa"
+#print(name3.capitalize())
+print(name3.count("k"))
+
