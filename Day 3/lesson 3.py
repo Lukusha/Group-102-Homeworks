5934a8706c459995063bdc88216e5752884febe0
Lukusha <luka.beraia808@gmail.com>
2023-02-14 14:11:32 +0400

    day3 commit

---
+++ Day 3/lesson 3.py
@@ -0,0 +1,121 @@
+# my_name = "Luka"
+
+#if 10>5:
+#    print(my_name)
+
+# if "a" in my_name:
+#     print("sheicavs a- s")
+
+
+# my_snetence = "aa {} bb {} cc {}".format("xx", "yy", "zz")
+
+# print(my_snetence)
+
+
+# my_name ="Luka"
+# my_lastname = "Beraia"
+# my_age = "18"
+
+# my_sentence0 = "aa {2} bb {1} cc {0}".format(my_name,my_lastname,my_age)
+
+# print(my_sentence0)
+
+                                    ########### CLASS WORK 1 ##########
+
+#  my_name ="Luka"
+# my_lastname = "Beraia"
+# my_age = "18"
+# my_height = "6"
+# my_weight = "74"
+
+# my_sentence = "My name is {} {} and im {} years old. I am {} ft tall and i weigh {} Kgs ".format(my_name,my_lastname,my_age,my_height,my_weight)
+
+# print(my_sentence)
+
+
+# my_name ="Luka"
+
+# if "e" in my_name :
+#     print("sheicavs e -s")
+# else:
+#         print("ar sheicavs e -s")
+
+
+### INPUT ###
+### OUTPUT ###
+
+
+# my_name = input("Enter your name : ") ### input methodi - input()
+# print("chemi saxelia " + my_name)
+
+
+# my_age = 22
+# my_age += 3
+# print(my_age)
+
+                                    ########## CLASS WORK 2 ###########
+
+
+# my_name = input("Enter Your name : ")
+# my_lastname = input("Enter your lastname : ")
+# (my_age) = input("Enter your age : ") #input yoveltvis stringi shemodis
+# new_age = int(my_age)
+
+# my_sentence = " Hi, Your name is {}, Your lastname is {} and you are {} years old.".format(my_name,my_lastname,my_age)
+# print(my_sentence)
+
+# year =input("Enter how many years : ")
+
+# new_age = int(my_age) + int(year)
+# print("*After {} Years*".format(year))
+
+
+# print("{} years went by. You are {} now.".format(year,new_age))
+
+
+
+                                    ######## CLASS WORK 3 ########
+
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# prod = num1 * num2
+# if prod>100 :
+#     print(prod)
+# else:
+#     print("You Lost")
+
+
+#print(20 % 6)  #Abrunebs Nashts 20/6 = 3 . 6*3 = 18. nashti rcheba 2.
+
+
+
+                                    ##########  HOMEWORK ###########
+
+
+
+num1 = int(input("Enter The First Number :"))
+num2 = int(input("Enter The Second Number : "))
+num3 = int(input("Enter The Third Number : "))
+sum = 0
+
+if num1 % 2 == 0 :
+    sum = sum
+else:
+    sum += num1
+
+if num2 % 2 == 0 :
+    sum = sum
+else:
+    sum += num2
+
+if num3 % 2 == 0 :
+    sum = sum
+else:
+    sum += num3
+
+print("Sum of the odd numbers you entered is : {} ".format(sum))
+
+
+
+
