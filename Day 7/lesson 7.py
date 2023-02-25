81b4502d7e5e2918ec8b7456e505a4f4d25db898
Lukusha <luka.beraia808@gmail.com>
2023-02-25 20:55:40 +0400

    7th commit

---
+++ Day 7/lesson 7.py
@@ -0,0 +1,70 @@
+# my_arr = [6,10.5,"Luka",True,False,-5,[1,1.5,2]]
+
+# print(my_arr[-1])
+# print(my_arr[4])
+# print(my_arr[3:6])
+
+
+
+
+# if "qababi" in menu :
+#     print("qababi gvaqvs")
+
+# menu[1] = "BBQ"
+# print(menu)
+
+# menu[2:4] = ["aa","bb","cc"]
+# print(menu)
+
+# menu = ["xinkali","mwvadi","qababi","xachapuri","lobiani","burgeri"]
+
+# # menu.insert(3, "nayini")
+# # print(menu)
+
+# menu.append("cecxli")
+# menu.append("navti")#appendi aucileblad siis bolos amatebs variables
+# print(menu)
+
+                        ########## Class Work ############
+
+kerdzebi = []
+
+
+# i = 0
+# while i < 5 :
+#    kerdzi = input("Sheiyvanet Kerdzi : ")
+#    if kerdzi.count("a")>=2 :
+#     kerdzebi.append(kerdzi)
+#    i+=1
+
+# print(kerdzebi)
+
+# for i in range(3) :
+#     kerdzi = input("sheiyvanet kerdzi : ")
+#     a_raodenoba = 0
+#     for char in kerdzi :
+#         if char == "a" :
+#             a_raodenoba +=1
+#     if a_raodenoba >= 2 :
+#         kerdzebi.append(kerdzi)
+#         a_raodenoba=0
+
+# print(kerdzebi)
+
+
+                    ########### CLASS WORK ############
+
+
+menu = ["xachapuri","lobiani","xinkali","mwvadi","lobio","qababi","blinebi"]
+new_menu =[]
+for food in menu :
+    if food[1] != "a" :
+        new_menu.append(food)
+
+print(new_menu)
+
+
+
+
+
+
---
+++ homework .py
@@ -0,0 +1,16 @@
+# scores =[10,15,43,32,12,3,41,51,19,9,11,18]
+# max = scores[0]
+
+# for i in scores :
+#     if i > max :
+#         max = i
+#     else :
+#         max = max
+    
+# print(max)
+
+
+students = ["nika","ana","mamuka","mate","andria","taso","Luka"]
+new_students =[]
+last = students[-1]
+print(last)
