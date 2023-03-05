683c34d15313e796dcab4c1dbf7a5b2c9116a239
Lukusha <luka.beraia808@gmail.com>
2023-03-05 20:20:24 +0400

    8th commit

---
+++ Day 8/lesson 8.py
@@ -0,0 +1,27 @@
+# name = "Lukusha"
+# scores = [20,30,15,4,7,8,6]
+
+# i = 0
+# while i < len(name) :
+#     print(name[i] + " - " + str(scores[i]))
+#     i += 1
+
+
+
+# def sum(num1,num2) :
+#     print(num1 + num2)
+
+# # sum(10,11)
+
+# my_arr =[2,4,6,8,10,12,14]
+
+# for num in my_arr :
+#    sum(num,1)
+
+
+def pair_sum(arr1,arr2) :
+    for i in range(len(arr1)):
+        print(arr1[i] + arr2[i])
+
+pair_sum([20,40,60],
+    [17,18,99])
